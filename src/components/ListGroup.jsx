5f6db6ea7dde7ce1b762683db5e27385193f08eb
Arpit Burra <arpitburra99@gmail.com>
2024-09-13 13:10:16 +0530

    add search filter functionality...

--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -1,29 +1,47 @@
-import React from 'react';
-import ListItems from './ListItems';
-import { useGetAllTodosQuery } from '../feature/todos/todoSlice';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import ListItems from "./ListItems";
+import { useGetAllTodosQuery } from "../feature/todos/todoSlice";
+import { NavLink } from "react-router-dom";
 
 const ListGroup = () => {
   const { data: todoList, isError, isLoading } = useGetAllTodosQuery();
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filteredTodos = todoList?.filter((todo) => {
+    return todo.title.toLowerCase().includes(searchQuery.toLowerCase());
+  });
 
   return (
-    <div className='container p-5'>
-      <div className='d-flex align-items-center justify-content-between'>
+    <div className="container p-5">
+      <div className="d-flex align-items-center justify-content-between">
         <h5>Todo List</h5>
-        <NavLink to={'/form'} className='btn btn-secondary'>
+        <div className="mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search todos..."
+            value={searchQuery}
+            onChange={handleSearchChange}
+          />
+        </div>
+        <NavLink to={"/form"} className="btn btn-secondary">
           Add Todo
         </NavLink>
       </div>
-      <div className='row'>
+      <div className="row">
         {isLoading ? (
-          <span className='text-center'>Loading...</span>
+          <span className="text-center">Loading...</span>
         ) : isError ? (
-          <span className='text-center'>Something Went Wrong!</span>
-        ) : todoList && todoList.length > 0 ? (
-          todoList.map((todo) => <ListItems key={todo._id} todo={todo} />)
+          <span className="text-center">Something Went Wrong!</span>
+        ) : filteredTodos && filteredTodos.length > 0 ? (
+          filteredTodos.map((todo) => <ListItems key={todo._id} todo={todo} />)
         ) : (
-          <div className='container p-5'>
-            <h5 className='text-center'>No Todo Data Yet!</h5>
+          <div className="container p-5">
+            <h5 className="text-center">No Todo Data Yet!</h5>
           </div>
         )}
       </div>
